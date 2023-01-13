2c33e64dfa5b0533575d9a6ddbe4987c1dbe9abd
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-13 15:16:48 +0100

    fix logo url

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -1,20 +1,16 @@
-// import { useNavigate } from "react-router";
-
 export default function Logo() {
-    // const navigate = useNavigate();
-
-    // const redirect = (event) => {
-    //     event.preventDefault();
-    //     navigate("/");
-    //     return;
-    // };
+    const redirect = (event) => {
+        event.preventDefault();
+        location.assign("/");
+        return;
+    };
 
     return (
         <img
             src="/olympics.jpg"
             alt="Logo"
             className="logo"
-            // onClick={redirect}
+            onClick={redirect}
         />
     );
 }
