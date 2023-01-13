522e32e21b4a58cb4ac576e0da6054f14cd1d96a
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-13 15:48:43 +0100

    add urls for users in FindPeople

--- client/src/components/FindPeople.tsx
+++ client/src/components/FindPeople.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function FindPeople() {
     const [newUsers, setNewUsers] = useState([]);
@@ -47,9 +48,13 @@ export default function FindPeople() {
             <div className="new-users">
                 {newUsers.map((user) => (
                     <div key={user.id}>
-                        <img src={user.imageurl} alt={user.firstname} />
+                        <Link to={`/user/${user.id}`}>
+                            <img src={user.imageurl} alt={user.firstname} />
+                        </Link>
                         <p>
-                            {user.firstname} {user.lastname}
+                            <Link to={`/user/${user.id}`}>
+                                {user.firstname} {user.lastname}
+                            </Link>
                         </p>
                     </div>
                 ))}
@@ -66,9 +71,13 @@ export default function FindPeople() {
                 <div className="found-users">
                     {foundUsers.map((user) => (
                         <div key={user.id}>
-                            <img src={user.imageurl} alt={user.firstname} />
+                            <Link to={`/user/${user.id}`}>
+                                <img src={user.imageurl} alt={user.firstname} />
+                            </Link>
                             <p>
-                                {user.firstname} {user.lastname}
+                                <Link to={`/user/${user.id}`}>
+                                    {user.firstname} {user.lastname}
+                                </Link>
                             </p>
                         </div>
                     ))}
