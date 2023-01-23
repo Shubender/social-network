8d8d80130a69b0d3ec874f0c2ff05e53440625f1
Andrii Verbytskyi <2andrey.verbitskiy@gmail.com>
2023-01-23 15:39:41 +0100

    made modal uploader

--- client/src/components/Uploader.tsx
+++ client/src/components/Uploader.tsx
@@ -2,35 +2,31 @@ import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import { useState, useEffect } from "react";
 
-export default class Uploader extends Component<any, any> {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    componentDidMount() {
-        // console.log("uploader mounted!");
-    }
+export default function Uploader(props) {
+    const [show, setShow] = useState(true);
 
-    render() {
-        return (
-            <div>
-                <Form onSubmit={this.props.handleSubmitUpload}>
-                    <Form.Group controlId="formFile" className="m-3 w-25">
-                        <Form.Label>Upload your avatar here:</Form.Label>
-                        <Form.Control
-                            type="file"
-                            name="file"
-                            accept="image/*"
-                            onChange={this.props.handleFileChange}
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" className="m-3">
-                        Upload
-                    </Button>
-                </Form>
-            </div>
-        );
-        // Here you will need to create a form to send a image file, just like in the Image Board
-    }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Form onSubmit={props.handleSubmitUpload}>
+                <Form.Group controlId="formFile" className="m-3 w-75">
+                    <Form.Label>Upload your avatar here:</Form.Label>
+                    <Form.Control
+                        type="file"
+                        name="file"
+                        accept="image/*"
+                        onChange={props.handleFileChange}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit" className="m-3">
+                    Upload
+                </Button>
+            </Form>
+        </Modal>
+    );
+    // Here you will need to create a form to send a image file, just like in the Image Board
 }
